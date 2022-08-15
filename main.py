4bda2fff78c4dff3f4f41653a6a0fc5ae843c7e9
vasilievi <vasilievi@outlook.com>
2022-08-15 09:45:45 +0300

    -

--- main.py
+++ main.py
@@ -2,32 +2,38 @@ import os, piexif
 from datetime import datetime
 
 ### SETTINGS ###
-dir = r"C:\Users\vasil\YandexDisk\photo\_photo\Италия (свадебное путешествие) 2011"
-newDate = datetime(2011, 9, 15, 0, 0, 0).strftime("%Y:%m:%d %H:%M:%S")
+ext = 'jpg'
+dir = r"C:\Users\vasil\YandexDisk\photo\20220509_Google Фото\Photos from 2018"
+newDate = datetime(2018, 1, 1, 0, 0, 0).strftime("%Y:%m:%d %H:%M:%S")
 ################
 
 
 now = datetime.now()
-incorrectDates = 0
+changedFiles = 0
 
 for filename in os.listdir(dir):
-    if filename[-3:].upper() == 'JPG':
+    if filename[-3:].upper() == ext.upper():
         picPath = dir + '\\' + filename
         exif_dict = piexif.load(picPath)
 
+        changeDate = False
         try:
             dateTimeOriginal = str(exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal])
             dateTimeOriginal = dateTimeOriginal[2:21]
             dateTimeOriginal = datetime.strptime(dateTimeOriginal, '%Y:%m:%d %H:%M:%S')
         except:
-            continue
+            print(filename + ': error')
+            changeDate = True
 
         if dateTimeOriginal > now:
-            incorrectDates += 1
-            print(filename + ' ' + str(dateTimeOriginal))
+            print(filename + ': ' + str(dateTimeOriginal))
+            changeDate = True
 
+        if changeDate:
+            changedFiles += 1
             exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = newDate
             exif_bytes = piexif.dump(exif_dict)
             piexif.insert(exif_bytes, picPath) ### SAVING PHOTO ###
+            print(filename + ': date changed to ' + str(newDate))
 
-print('Incorrect dates: ' + str(incorrectDates))
+print('Changed files: ' + str(changedFiles))
