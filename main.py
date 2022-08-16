68df61fe72eb96892451940f24ffd93fe72fd528
vasilievi <vasilievi@outlook.com>
2022-08-16 08:06:58 +0300

    logging

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 ### SETTINGS ###
 ext = 'jpg'
-dir = r"C:\Users\vasil\YandexDisk\photo\2011-08_Девишник"
+dir = r"C:\Users\vasil\YandexDisk\photo\2010-04-17_Ксюша приехала\haloween 2010"
 logFile = r"C:\temp\pyLog.txt"
-newDate = datetime(2011, 8, 1, 0, 0, 0).strftime("%Y:%m:%d %H:%M:%S")
+newDate = datetime(2010, 10, 1, 0, 0, 0).strftime("%Y:%m:%d %H:%M:%S")
 ################
 
 def printToFile(text):
@@ -21,9 +21,10 @@ changedFiles = 0
 for root, dirs, files in os.walk(dir):
     for file in files:
         picPath = os.path.join(root,file)
-        
+        printToFile(picPath)
+
         if picPath[-3:].upper() != ext.upper():
-            printToFile(picPath + ': is not ' + ext)
+            printToFile('   File is not ' + ext)
             continue
         
         exif_dict = piexif.load(picPath)
@@ -35,11 +36,11 @@ for root, dirs, files in os.walk(dir):
             dateTimeOriginal = datetime.strptime(dateTimeOriginalString, '%Y:%m:%d %H:%M:%S')
         except:
             dateTimeOriginal = datetime.strptime(newDate, '%Y:%m:%d %H:%M:%S')
-            printToFile(picPath + ': error reading date')
+            printToFile('   Error reading date')
             changeDate = True
 
         if dateTimeOriginal > now:
-            printToFile(picPath + ': incorrect date ' + str(dateTimeOriginal))
+            printToFile('   Incorrect date ' + str(dateTimeOriginal))
             changeDate = True
 
         if changeDate:
@@ -47,7 +48,7 @@ for root, dirs, files in os.walk(dir):
             exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = newDate
             exif_bytes = piexif.dump(exif_dict)
             piexif.insert(exif_bytes, picPath) ### SAVING PHOTO ###
-            printToFile(picPath + ': date changed to ' + str(newDate))
+            printToFile('   Date changed to ' + str(newDate))
 
 printToFile('Changed files: ' + str(changedFiles))
 
